83af2123f343480f3c2dfa6e91a5acb145ac6723
DSV <denislavveizov@gmail.com>
2016-07-05 21:37:16 +0300

    work

---
+++ class/PragmaticJavascriptObjectsDenislav/ex1/ex1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Denislav on 5.7.2016 г..
+ */
+function person (age,hairColor) {
+    return{age:age,hairColor: hairColor}
+}
+
+var hairColorPesho = "black";
+var agePesho = 21;
+
+var pesho = person(agePesho,hairColorPesho);
+
+console.log(pesho);
