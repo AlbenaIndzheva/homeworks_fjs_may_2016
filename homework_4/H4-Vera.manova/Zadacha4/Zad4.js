a5c17bc17a9834bf11f313b0a6fde070184ffbcf
crimson <vera.manova@yahoo.com>
2016-07-05 19:25:07 +0300

    H4

---
+++ homework_4/H4-Vera.manova/Zadacha4/Zad4.js
@@ -0,0 +1,10 @@
+"use strict"
+
+/* Вера Манова - Write a function to count the number of divs on the web page
+
+    */
+
+
+   var a = document.getElementById("main"); 
+   var b = a.getElementsByTagName("div"); 
+    console.log(b);
