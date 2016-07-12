42449d0fe87397ffe5a1757ca935c4726c8074fc
Borislav Yordanov <brobos@gmail.com>
2016-07-12 22:42:48 +0300

    Create scriptThree.js

---
+++ homework_1_control_structures/Borislav/js/scriptThree.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var а, b;
+    a = document.getElementById("one").value;
+    b = document.getElementById("two").value;
+    document.getElementById("demo").innerHTML = b + " " + a;
+}
