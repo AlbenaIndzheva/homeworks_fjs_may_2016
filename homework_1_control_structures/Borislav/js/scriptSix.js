208d1a0581e1f94c2969f280827008b314ed4a92
Borislav Yordanov <brobos@gmail.com>
2016-07-12 22:45:22 +0300

    Create scriptSix.js

---
+++ homework_1_control_structures/Borislav/js/scriptSix.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var а, b, c;
+    a = document.getElementById("one").value;
+    b = document.getElementById("two").value;
+    c = document.getElementById("three").value;
+
+    document.getElementById("demo").innerHTML = "a1 = " + b + ";" + " a2 = " + c + ";" + " a3 = " + a;
+
+}
